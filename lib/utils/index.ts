43c486277bdff42999f18a71a4627585461e4762
Roy Li <me@dada.li>
2019-10-08 16:53:37 +0800

    feat: add support for clashr

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -499,6 +499,20 @@ export const getClashNodes = (
             } : null),
           };
 
+        case NodeTypeEnum.Shadowsocksr:
+          return {
+            type: 'ssr',
+            name: nodeConfig.nodeName,
+            server: nodeConfig.hostname,
+            port: nodeConfig.port,
+            password: nodeConfig.password,
+            obfs: nodeConfig.obfs,
+            obfsparam: nodeConfig.obfsparam,
+            protocol: nodeConfig.protocol,
+            protocolparam: nodeConfig.protoparam,
+            cipher: nodeConfig.method,
+          };
+
         // istanbul ignore next
         default:
           console.info(`${nodeConfig.type} is not supported yet, ${nodeConfig.nodeName} will be ignored.`);
