be7d237585a94e46a4103c8ae0762eb484c60858
Roy Li <me@dada.li>
2019-10-31 20:02:04 +0800

    deps: upgrade

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['eslint:recommended'],
+  env: {
+    es6: true,
+    node: true
+  },
+  rules: {
+    "array-bracket-spacing": 0,
+    "comma-dangle": 0,
+    "dot-notation": 0,
+    "valid-jsdoc": 0
+  }
+};
