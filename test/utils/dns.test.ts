b96489b6f92daa9e87f3863fb80e97b4af225fa6
Roy Li <me@dada.li>
2020-05-05 14:44:43 +0800

    feat: 支持解析 ipv6 地址

--- test/utils/dns.test.ts
+++ test/utils/dns.test.ts
@@ -2,7 +2,13 @@ import test from 'ava';
 import { isIp } from '../../lib/utils';
 import * as dns from '../../lib/utils/dns';
 
-test('resolveDomain', async t => {
+test('resolveDomain ipv4', async t => {
   const ips = await dns.resolveDomain('gstatic.com');
   t.true(isIp(ips[0]));
 });
+
+test('resolveDomain ipv6', async t => {
+  const ips = await dns.resolveDomain('ipv6.lookup.test-ipv6.com');
+  t.true(isIp(ips[0]));
+});
+
