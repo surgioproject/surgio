1cd8436a6335a4f018fc96532f297502cec8ce3b
Roy Li <me@dada.li>
2020-05-05 15:06:40 +0800

    fix: SSR URI 中包含 ipv6 地址解析错误

--- test/utils/ssr.test.ts
+++ test/utils/ssr.test.ts
@@ -77,3 +77,22 @@ test('parseSSRUri 4 - standard base64', t => {
     obfsparam: ''
   });
 });
+
+test('parseSSRUri ipv6', t => {
+  // 2404:6800:4005:801::2003:80:auth_aes128_sha1:chacha20:plain:ZXhhbXBsZQ==/?obfsparam=&protoparam=MTIzNDU2ICAgICA6MTIzNDVxd2VydHl1&remarks=8J+Hr/Cfh7UgSlA=&group=U3VyZ2lv
+  const uri = 'ssr://MjQwNDo2ODAwOjQwMDU6ODAxOjoyMDAzOjgwOmF1dGhfYWVzMTI4X3NoYTE6Y2hhY2hhMjA6cGxhaW46WlhoaGJYQnNaUT09Lz9vYmZzcGFyYW09JnByb3RvcGFyYW09TVRJek5EVTJJQ0FnSUNBNk1USXpORFZ4ZDJWeWRIbDEmcmVtYXJrcz04SitIci9DZmg3VWdTbEE9Jmdyb3VwPVUzVnlaMmx2';
+  const conf = parseSSRUri(uri);
+
+  t.deepEqual(conf, {
+    type: NodeTypeEnum.Shadowsocksr,
+    nodeName: '🇯🇵 JP',
+    hostname: '2404:6800:4005:801::2003',
+    port: '80',
+    protocol: 'auth_aes128_sha1',
+    method: 'chacha20',
+    obfs: 'plain',
+    password: 'example',
+    protoparam: '123456:12345qwertyu',
+    obfsparam: ''
+  });
+});
