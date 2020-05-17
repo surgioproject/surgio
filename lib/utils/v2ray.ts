4491e60cb5e0709056d93c82d314d23ec482745a
Roy Li <me@dada.li>
2020-05-17 14:33:51 +0800

    fix(utils:v2ray): lack of transport settings
    #91

--- lib/utils/v2ray.ts
+++ lib/utils/v2ray.ts
@@ -3,16 +3,33 @@ import queryString from 'query-string';
 
 // https://github.com/v2ray/v2ray-core/issues/1569
 export const formatVmessUri = (nodeConfig: VmessNodeConfig): string => {
-  const uri = [
+  const uri: string[] = [
     nodeConfig.uuid,
     '@',
     `${nodeConfig.hostname}:${nodeConfig.port}`,
     (nodeConfig.path || '/'),
   ];
-  const queries = {
+  const queries: any = {
     network: nodeConfig.network,
     tls: nodeConfig.tls,
   };
+
+  if (nodeConfig.skipCertVerify) {
+    queries['tls.allowInsecure'] = true;
+  }
+
+  if (nodeConfig.network === 'ws') {
+    if (typeof nodeConfig.wsHeaders !== 'undefined') {
+      Object.keys(nodeConfig.wsHeaders).forEach(key => {
+        if (!/host/i.test(key)) {
+          queries[`ws.headers.${key}`] = nodeConfig.wsHeaders![key];
+        }
+      });
+    }
+    if (nodeConfig.host) {
+      queries[`ws.headers.host`] = nodeConfig.host;
+    }
+  }
 
   return `vmess://${uri.join('')}?${queryString.stringify(queries)}`;
 };
