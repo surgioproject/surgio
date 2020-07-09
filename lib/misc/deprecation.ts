9e0ddf7118931009951dc1f21943d3340256885d
Roy Li <me@dada.li>
2020-07-09 13:01:30 +0800

    feat: filter 行为修改
    BREAKING CHANGE: useProviders, discardProviders 默认为严格模式

--- lib/misc/deprecation.ts
+++ lib/misc/deprecation.ts
@@ -1,4 +1,2 @@
-export const DEP003 = 'useProvider 将在之后版本改为默认使用严格模式';
-export const DEP004 = 'discardProviders 将在之后版本改为默认使用严格模式';
 export const DEP005 = 'surgioConfig.surgeConfig.shadowsocksFormat: "custom" 已不推荐使用，请改为 "ss"';
 export const DEP006 = 'surgioConfig.surgeConfig.v2ray: "external" 已不推荐使用，请改为 "native"';
