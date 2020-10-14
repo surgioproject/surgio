8ee0f4d7cd1138abef53433ee37e43c7ad4d1840
Roy Li <me@dada.li>
2020-10-14 16:52:35 +0800

    feat: dns resolve should have a timeout

--- lib/utils/constant.ts
+++ lib/utils/constant.ts
@@ -1,16 +1,28 @@
 // istanbul ignore file
 
-export const NETWORK_TIMEOUT = process.env.SURGIO_NETWORK_TIMEOUT ? Number(process.env.SURGIO_NETWORK_TIMEOUT) : 5000;
+export const NETWORK_TIMEOUT = process.env.SURGIO_NETWORK_TIMEOUT
+  ? Number(process.env.SURGIO_NETWORK_TIMEOUT)
+  : 5000;
 
-export const NETWORK_CONCURRENCY = process.env.SURGIO_NETWORK_CONCURRENCY ? Number(process.env.SURGIO_NETWORK_CONCURRENCY) : 5;
+export const NETWORK_RESOLVE_TIMEOUT = process.env
+  .SURGIO_NETWORK_RESOLVE_TIMEOUT
+  ? Number(process.env.SURGIO_NETWORK_RESOLVE_TIMEOUT)
+  : 3000;
 
-export const NETWORK_RETRY = process.env.SURGIO_NETWORK_RETRY ? Number(process.env.SURGIO_NETWORK_RETRY) : 1;
+export const NETWORK_CONCURRENCY = process.env.SURGIO_NETWORK_CONCURRENCY
+  ? Number(process.env.SURGIO_NETWORK_CONCURRENCY)
+  : 5;
+
+export const NETWORK_RETRY = process.env.SURGIO_NETWORK_RETRY
+  ? Number(process.env.SURGIO_NETWORK_RETRY)
+  : 1;
 
 export const NETWORK_CLASH_UA = process.env.SURGIO_NETWORK_CLASH_UA ?? 'clash';
 
 export const NETWORK_SURGIO_UA = 'surgio';
 
-export const OBFS_UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1';
+export const OBFS_UA =
+  'Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1';
 
 export const PROXY_TEST_URL = 'http://cp.cloudflare.com/generate_204';
 
@@ -54,19 +66,29 @@ export const LOON_SUPPORTED_RULE: ReadonlyArray<string> = [
   'IP-CIDR',
   'GEOIP',
   'FINAL',
-]
-
-export const MELLOW_UNSUPPORTED_RULE: ReadonlyArray<string> = [
-  'URL-REGEX', 'USER-AGENT', 'AND', 'OR', 'NOT', 'DEST-PORT', 'IN-PORT', 'SRC-IP', 'RULE-SET'
 ];
 
-export const REMOTE_SNIPPET_CACHE_MAXAGE: number = process.env.SURGIO_REMOTE_SNIPPET_CACHE_MAXAGE ?
-  Number(process.env.SURGIO_REMOTE_SNIPPET_CACHE_MAXAGE) :
-  1000 * 60 * 60 * 12; // 12h
+export const MELLOW_UNSUPPORTED_RULE: ReadonlyArray<string> = [
+  'URL-REGEX',
+  'USER-AGENT',
+  'AND',
+  'OR',
+  'NOT',
+  'DEST-PORT',
+  'IN-PORT',
+  'SRC-IP',
+  'RULE-SET',
+];
 
-export const PROVIDER_CACHE_MAXAGE: number = process.env.SURGIO_PROVIDER_CACHE_MAXAGE ?
-  Number(process.env.SURGIO_PROVIDER_CACHE_MAXAGE) :
-  10 * 60 * 1000; // 10min
+export const REMOTE_SNIPPET_CACHE_MAXAGE: number = process.env
+  .SURGIO_REMOTE_SNIPPET_CACHE_MAXAGE
+  ? Number(process.env.SURGIO_REMOTE_SNIPPET_CACHE_MAXAGE)
+  : 1000 * 60 * 60 * 12; // 12h
+
+export const PROVIDER_CACHE_MAXAGE: number = process.env
+  .SURGIO_PROVIDER_CACHE_MAXAGE
+  ? Number(process.env.SURGIO_PROVIDER_CACHE_MAXAGE)
+  : 10 * 60 * 1000; // 10min
 
 export const CATEGORIES = {
   SNIPPET: 'Snippet',
@@ -81,6 +103,7 @@ export const CATEGORIES = {
 
 export const RELAY_SERVICE = 'https://surgio-cors.herokuapp.com/';
 
-export const ERR_INVALID_FILTER = '传入的过滤器无效，请检查语法和变量名是否正确';
+export const ERR_INVALID_FILTER =
+  '传入的过滤器无效，请检查语法和变量名是否正确';
 
 export const TMP_FOLDER_NAME = 'surgio-config';
--- test/utils/dns.test.ts
+++ test/utils/dns.test.ts
@@ -2,13 +2,17 @@ import test from 'ava';
 import { isIp } from '../../lib/utils';
 import * as dns from '../../lib/utils/dns';
 
-test('resolveDomain ipv4', async t => {
+test('resolveDomain ipv4', async (t) => {
   const ips = await dns.resolveDomain('gstatic.com');
   t.true(isIp(ips[0]));
 });
 
-test('resolveDomain ipv6', async t => {
+test('resolveDomain ipv6', async (t) => {
   const ips = await dns.resolveDomain('ipv6.lookup.test-ipv6.com');
   t.true(isIp(ips[0]));
 });
 
+test('resolveDomain timeout', async (t) => {
+  const ips = await dns.resolveDomain('www.gstatic.com', 1);
+  t.is(ips.length, 0);
+});
