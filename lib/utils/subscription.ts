e58e5ef70b135b71bab0790fdcb95dd24ac6aae1
RulesW <97848291+RulesW@users.noreply.github.com>
2022-12-30 23:17:37 +0800

    [fix] Incorrect subscription info.

--- lib/utils/subscription.ts
+++ lib/utils/subscription.ts
@@ -67,11 +67,11 @@ export const formatSubscriptionUserInfo = (
   readonly expire: string;
 } => {
   return {
-    upload: filesize(userInfo.upload),
-    download: filesize(userInfo.download),
-    used: filesize(userInfo.upload + userInfo.download),
-    left: filesize(userInfo.total - userInfo.upload - userInfo.download),
-    total: filesize(userInfo.total),
+    upload: filesize(userInfo.upload, {base:2}),
+    download: filesize(userInfo.download, {base:2}),
+    used: filesize(userInfo.upload + userInfo.download, {base:2}),
+    left: filesize(userInfo.total - userInfo.upload - userInfo.download, {base:2}),
+    total: filesize(userInfo.total, {base:2}),
     expire: userInfo.expire
       ? `${format(
           new Date(userInfo.expire * 1000),
