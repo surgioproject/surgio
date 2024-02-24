8ed380a02a6a091f7c4bd21d93d2bd7abb496882
Roy Li <me@royli.dev>
2024-02-24 13:08:38 +0800

    refactor: change vmess validator

--- src/validators/vmess.ts
+++ src/validators/vmess.ts
@@ -7,19 +7,30 @@ import {
   AlterIdValiator,
 } from './common'
 
+export const VmessNetworkValidator = z.union([
+  z.literal('tcp'),
+  z.literal('ws'),
+  // z.literal('h2'),
+  // z.literal('http'),
+  // z.literal('grpc'),
+])
+
+export const VmessMethodValidator = z.union([
+  z.literal('none'),
+  z.literal('aes-128-gcm'),
+  z.literal('chacha20-ietf-poly1305'),
+  z.literal('auto'),
+])
+
+// https://stash.wiki/proxy-protocols/proxy-types#vmess
 export const VmessNodeConfigValidator = SimpleNodeConfigValidator.extend({
   type: z.literal(NodeTypeEnum.Vmess),
   hostname: z.string(),
   port: PortValidator,
-  method: z.union([
-    z.literal('none'),
-    z.literal('aes-128-gcm'),
-    z.literal('chacha20-ietf-poly1305'),
-    z.literal('auto'),
-  ]),
+  method: VmessMethodValidator,
   uuid: z.string().uuid(),
   alterId: AlterIdValiator,
-  network: z.union([z.literal('tcp'), z.literal('ws')]),
+  network: VmessNetworkValidator,
   tls: z.boolean(),
   host: z.ostring(),
   path: z.ostring(),
